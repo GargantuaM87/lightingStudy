581b80cd83cb15243442f448b9bbc5b10ec476ad
NkosiDaniel <xxxenouniverse@gmail.com>
2026-03-17 13:35:12 -0400

    Point Shadows implemented

--- assets/shaders/default.frag
+++ assets/shaders/default.frag
@@ -22,10 +22,7 @@ struct DirectionalLight {
 
 struct PointLight {
     vec3 position;
-
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    vec3 color;
 
     float constant;
     float linear;
@@ -36,12 +33,15 @@ uniform PointLight pLight;
 uniform Material u_mat;
 uniform vec3 u_viewPos;
 uniform sampler2D depthMap;
+uniform samplerCube depthCubeMap;
+uniform float far_plane;
 
 vec3 color;
 
 vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 vec3 CalcDirLight(DirectionalLight light, vec3 normal, vec3 viewDir);
 float ShadowCalculation(vec4 fragPosLightSpace, float bias);
+float PointShadowCalculation(vec3 fragPos);
 
 void main()
 {
@@ -49,8 +49,8 @@ void main()
     vec3 viewDir = normalize(u_viewPos - fs_in.FragPos);
     color = texture(u_mat.texture_diffuse1, fs_in.TexCoords).rgb;
 
-    vec3 result = CalcDirLight(dLight, normal, viewDir);
-    //result += CalcPointLight(pLight, normal, fs_in.FragPos, viewDir);
+    //result = CalcDirLight(dLight, normal, viewDir);
+    vec3 result = CalcPointLight(pLight, normal, fs_in.FragPos, viewDir);
     FragColor = vec4(result, 1.0);
 }
 
@@ -84,9 +84,9 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
     // specular
     float spec = pow(max(dot(viewDir, halfwayDir), 0.0), 32.0);
 
-    vec3 ambient = light.ambient * color;
-    vec3 diffuse = light.diffuse * diff * color;
-    vec3 specular = light.specular * spec;
+    vec3 ambient = 0.3 * color;
+    vec3 diffuse = diff * color;
+    vec3 specular = light.color * spec;
 
     // attenuation
     float distance = length(light.position - fragPos);
@@ -96,7 +96,11 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
     diffuse *= attenuation;
     specular *= attenuation;
 
-    return (ambient + diffuse + specular);
+    // calculating shadow
+    float shadow = PointShadowCalculation(fragPos);
+    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;
+
+    return lighting;
 }
 
 float ShadowCalculation(vec4 fragPosLightSpace, float bias) {
@@ -107,6 +111,32 @@ float ShadowCalculation(vec4 fragPosLightSpace, float bias) {
     float closestDepth = texture(depthMap, projCoords.xy).r;
     float currentDepth = projCoords.z; // the depth of the current fragment the fragment shader is running on
     // depth comparison
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(depthMap, 0);
+    for (int x = -1; x <= 1; ++x)
+    {
+        for (int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(depthMap, projCoords.xy + vec2(x, y) * texelSize).r;
+            shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0;
+        }
+    }
+    shadow /= 9.0;
+
+    if (projCoords.z > 1.0)
+        shadow = 0.0;
+
+    return shadow;
+}
+
+float PointShadowCalculation(vec3 fragPos) {
+    vec3 fragToLight = fragPos - pLight.position; // from the lightPos to fragPos
+    // normalized depth value between light source and its closest visible fragment
+    float closestDepth = texture(depthCubeMap, fragToLight).r;
+    closestDepth *= far_plane; // transform range from [0,1] to [0, far_plane];
+    float currentDepth = length(fragToLight);
+    float bias = 0.05;
     float shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+
     return shadow;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <GLFW/glfw3.h>
 #include <cmath>
 #include <filesystem>
+#include <string>
 
 #include "../headers/shaderClass.h"
 #include "../headers/EBO.h"
@@ -21,6 +22,7 @@
 #include "../include/glm/glm.hpp"
 #include "../include/glm/gtc/matrix_transform.hpp"
 #include "../include/glm/gtc/type_ptr.hpp"
+
 
 const unsigned int width = 800;
 const unsigned int height = 800;
@@ -64,6 +66,8 @@ int main(int, char **)
      // Parses the fragment and vertex shader files and wraps them into a shader program
      // The files are compiled to an intermediary language then translated into specific instructions for the GPU
      Shader depthShader("../assets/shaders/shadows/depth.vert", "../assets/shaders/shadows/depth.frag");
+     Shader pointDepthShader("../assets/shaders/shadows/pointShadow.vert", "../assets/shaders/shadows/pointShadow.frag");
+     pointDepthShader.LinkGeometry("../assets/shaders/shadows/pointShadow.geom");
      Shader framebufferShader("../assets/shaders/framebuffer.vert", "../assets/shaders/framebuffer.frag");
      Shader shadowShader("../assets/shaders/shadows/shadow.vert", "../assets/shaders/default.frag");
      // Models
@@ -82,23 +86,40 @@ int main(int, char **)
      const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
      unsigned int depthMapFBO;
      glGenFramebuffers(1, &depthMapFBO);
-
+     // DEPTH TEXTURE MAP
      unsigned int depthMap;
      glGenTextures(1, &depthMap);
      glBindTexture(GL_TEXTURE_2D, depthMap);
      glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
      glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+     float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f};
+     glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
 
      glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
      glDrawBuffer(GL_NONE);
      glReadBuffer(GL_NONE);
      glBindFramebuffer(GL_FRAMEBUFFER, 0);
+     // DEPTH CUBE MAP
+     unsigned int depthCubemap;
+     glGenTextures(1, &depthCubemap);
+
+     glBindTexture(GL_TEXTURE_CUBE_MAP, depthCubemap);
+     for (unsigned int i = 0; i < 6; ++i)
+             glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_DEPTH_COMPONENT,
+                          SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+     glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+     glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthCubemap, 0);
+     glBindFramebuffer(GL_FRAMEBUFFER, 0);
      ///---END OF SHADOW VARIABLES---
-     //FBO fbo();
       //-----------END OF IMAGE VARIABLES-----------
 
      glEnable(GL_DEPTH_TEST); // Allows for depth comparison and updates the depth buffer
@@ -126,6 +147,7 @@ int main(int, char **)
 
      shadowShader.Activate();
      shadowShader.SetToInt("depthMap", 1);
+     shadowShader.SetToInt("depthCubeMap", 2);
 
      // Main Render Loop
      while (!glfwWindowShouldClose(window))
@@ -150,8 +172,10 @@ int main(int, char **)
           //---FIRST PASS (SHADOW MAP)---
           glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
           glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+          glBindTexture(GL_TEXTURE_2D, depthMap);
+          glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
           glClear(GL_DEPTH_BUFFER_BIT);
-
+          // 1. Depth Texture Map
           float nearPlane = 1.0f, farPlane = 7.5f;
           glm::mat4 model(1.0f);
           glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, nearPlane, farPlane);
@@ -162,6 +186,37 @@ int main(int, char **)
           depthShader.SetToMat4("lightSpaceMatrix", lightSpaceMatrix);
           depthShader.SetToMat4("model", model);
           cafe.Draw(depthShader);
+          // 2. Depth Cube Map
+          glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+          glBindTexture(GL_TEXTURE_CUBE_MAP, depthCubemap);
+          glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthCubemap, 0);
+          glClear(GL_DEPTH_BUFFER_BIT);
+          float aspect = (float)SHADOW_WIDTH/(float)SHADOW_HEIGHT;
+          float near = 1.0f;
+          float far = 25.0f;
+          glm::mat4 shadowProj = glm::perspective(glm::radians(90.0f), aspect, near, far);
+          std::vector<glm::mat4> shadowTransforms;
+          shadowTransforms.push_back(shadowProj *
+                           glm::lookAt(GUI.lightPos, GUI.lightPos + glm::vec3( 1.0, 0.0, 0.0), glm::vec3(0.0,-1.0, 0.0)));
+          shadowTransforms.push_back(shadowProj *
+                           glm::lookAt(GUI.lightPos, GUI.lightPos + glm::vec3(-1.0, 0.0, 0.0), glm::vec3(0.0,-1.0, 0.0)));
+          shadowTransforms.push_back(shadowProj *
+                           glm::lookAt(GUI.lightPos, GUI.lightPos + glm::vec3( 0.0, 1.0, 0.0), glm::vec3(0.0, 0.0, 1.0)));
+          shadowTransforms.push_back(shadowProj *
+                           glm::lookAt(GUI.lightPos, GUI.lightPos + glm::vec3( 0.0,-1.0, 0.0), glm::vec3(0.0, 0.0,-1.0)));
+          shadowTransforms.push_back(shadowProj *
+                           glm::lookAt(GUI.lightPos, GUI.lightPos + glm::vec3( 0.0, 0.0, 1.0), glm::vec3(0.0,-1.0, 0.0)));
+          shadowTransforms.push_back(shadowProj *
+                           glm::lookAt(GUI.lightPos, GUI.lightPos + glm::vec3( 0.0, 0.0,-1.0), glm::vec3(0.0,-1.0, 0.0)));
+          pointDepthShader.Activate();
+          pointDepthShader.SetToMat4("model", model);
+          for(int i = 0; i < 6; ++i)
+          {
+              pointDepthShader.SetToMat4(&("shadowMatrices[" + std::to_string(i) + "]")[0], shadowTransforms[i]);
+          }
+          pointDepthShader.SetToVec3("lightPos", &GUI.lightPos[0]);
+          pointDepthShader.SetToFloat("far_plane", farPlane);
+          cafe.Draw(pointDepthShader);
           glBindFramebuffer(GL_FRAMEBUFFER, 0);
           //---END OF FIRST PASS---
           //---SECOND PASS (SHADOW RENDERING)---
@@ -174,23 +229,24 @@ int main(int, char **)
           shadowShader.Activate();
           glActiveTexture(GL_TEXTURE1);
           glBindTexture(GL_TEXTURE_2D, depthMap);
+          glActiveTexture(GL_TEXTURE2);
+          glBindTexture(GL_TEXTURE_CUBE_MAP, depthCubemap);
 
           shadowShader.SetToMat4("view", view);
           shadowShader.SetToMat4("projection", projection);
           shadowShader.SetToMat4("model", model);
+          shadowShader.SetToFloat("far_plane", farPlane);
           shadowShader.SetToMat4("lightSpaceMatrix", lightSpaceMatrix);
           shadowShader.SetToVec3("u_viewPos", &camera.Position[0]);
           shadowShader.SetToVec3("dLight.direction", &GUI.lightDir[0]);
           shadowShader.SetToVec3("dLight.ambient", &glm::vec3(0.4)[0]);
           shadowShader.SetToVec3("dLight.diffuse", &glm::vec3(0.4)[0]);
           shadowShader.SetToVec3("dLight.specular", &glm::vec3(0.2)[0]);
-          /*shadowShader.SetToVec3("pLight.position", &lightPos[0]);
-          shadowShader.SetToVec3("pLight.ambient", &glm::vec3(0.6)[0]);
-          shadowShader.SetToVec3("pLight.diffuse", &glm::vec3(0.3)[0]);
-          shadowShader.SetToVec3("pLight.specular", &glm::vec3(0.45)[0]);
+          shadowShader.SetToVec3("pLight.position", &GUI.lightPos[0]);
+          shadowShader.SetToVec3("pLight.color", &glm::vec3(0.8)[0]);
           shadowShader.SetToFloat("pLight.constant", 1.0f);
           shadowShader.SetToFloat("pLight.linear", 0.32f);
-          shadowShader.SetToFloat("pLight.quadratic", 0.09f);*/
+          shadowShader.SetToFloat("pLight.quadratic", 0.09f);
           cafe.Draw(shadowShader);
 
           model = glm::translate(model, GUI.lightPos);
